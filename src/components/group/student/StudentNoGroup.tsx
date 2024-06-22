8fa6a75034c5d12232d6ec3b632737dc9af8b8a8
고승희 <bookshelf1234@ewhain.net>
2024-06-23 04:29:48 +0900

    Feat: 학생 그룹조회 - 가입한 그룹 없는 경우 화면

--- src/components/group/student/StudentNoGroup.tsx
+++ src/components/group/student/StudentNoGroup.tsx
@@ -1,3 +1,20 @@
+import Jellyfish from '@assets/images/group/jellyfish.svg';
+
 export default function StudentNoGroup() {
-  return <div>StudentNoGroup</div>;
+  return (
+    <div
+      className="no-group-section"
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <img src={Jellyfish} alt="해파리" style={{ width: '50%' }} />
+      <h1 style={{ fontSize: '1.5rem', marginTop: '2rem' }}>아직 가입한 그룹이 없어요.</h1>
+    </div>
+  );
 }
