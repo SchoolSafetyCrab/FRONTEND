622637e286cd61b2d207f4054445922b9b06d8a7
고승희 <bookshelf1234@ewhain.net>
2024-06-23 15:13:55 +0900

    Feat: 비밀번호 모달창 화면구현

--- src/components/group/student/GroupBox.tsx
+++ src/components/group/student/GroupBox.tsx
@@ -1,8 +1,41 @@
+import React, { useState } from 'react';
 import School from '@assets/images/group/school.svg';
 import People from '@assets/images/group/people.svg';
 import '@styles/group/GroupBox.css';
 
 export default function GroupBox() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [inputPassword, setInputPassword] = useState('');
+  // eslint-disable-next-line no-unused-vars
+  const [isPasswordCorrect, setIsPasswordCorrect] = useState<boolean | null>(null);
+
+  const correctPassword = '1234'; // 실제 비밀번호로 교체
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    setInputPassword('');
+    setIsPasswordCorrect(null);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputPassword(e.target.value);
+  };
+
+  const handlePasswordSubmit = () => {
+    if (inputPassword === correctPassword) {
+      alert('가입이 완료되었습니다.');
+      handleModalClose();
+    } else {
+      alert('비밀번호가 틀렸습니다.');
+      setIsPasswordCorrect(false);
+      handleModalClose();
+    }
+  };
+
   return (
     <div className="box-container">
       <div className="name-div">
@@ -17,8 +50,37 @@ export default function GroupBox() {
         <h3>26/32</h3>
       </div>
       <div className="button-div">
-        <button type="button">가입하기</button>
+        <button type="button" onClick={handleModalOpen}>
+          가입하기
+        </button>
       </div>
+
+      {isModalOpen && (
+        <div className="password-modal-container">
+          <div className="background-div" />
+          <div className="modal-contents">
+            <h1 style={{ fontSize: '1rem' }}>비밀번호를 입력해주세요.</h1>
+            <input
+              type="password"
+              className="form-control"
+              id="group-password"
+              value={inputPassword}
+              onChange={handlePasswordChange}
+              style={{
+                fontSize: '1.5rem',
+                width: '100%',
+                height: '2rem',
+                border: '1px solid #FFD568',
+                marginBottom: '1rem',
+                textAlign: 'center',
+              }}
+            />
+            <button type="button" onClick={handlePasswordSubmit}>
+              확인
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
