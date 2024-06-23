0a36e3c6e0b85c1cf988904e568e6b1d46f1e96d
limjonghyeok <jjognbbang2@naver.com>
2024-06-24 04:02:08 +0900

    내 위치 저장

--- src/store/userInfo/UserInfo.tsx
+++ src/store/userInfo/UserInfo.tsx
@@ -1,11 +1,12 @@
 import { atomWithImmer } from 'jotai-immer';
 
 interface UserInfo {
+  id: string;
   nickName: string;
   userImg: string;
   role: string;
 }
 
-const userInfoAtom = atomWithImmer<UserInfo>({ nickName: '', userImg: '', role: '' });
+const userInfoAtom = atomWithImmer<UserInfo>({ id: '', nickName: '', userImg: '', role: '' });
 
 export default userInfoAtom;
