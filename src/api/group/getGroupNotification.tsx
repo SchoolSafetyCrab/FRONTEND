20e6393aa6da759392881a0265f7fa109173cc32
고승희 <bookshelf1234@ewhain.net>
2024-06-25 21:21:11 +0900

    Feat: 공지조회 api 연결

---
+++ src/api/group/getGroupNotification.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import API_BASE_URL from '../Apiconfig';
+import { MessageInfo } from '../../interfaces/MessageInfo';
+
+const getGroupNotification = async (groupId: number): Promise<MessageInfo[] | null> => {
+  const accessToken = localStorage.getItem('ACCESS_TOKEN');
+
+  try {
+    const response = await axios.get(`${API_BASE_URL}api/common/find/notification/${groupId}`, {
+      headers: {
+        Authorization: `${accessToken}`,
+      },
+    });
+    const result = response.data.data;
+    return result;
+  } catch (err) {
+    console.log(err);
+    if (axios.isAxiosError(err)) {
+      alert(err.response?.data?.errorMessage || '알 수 없는 오류가 발생했습니다.');
+    } else {
+      alert('알 수 없는 오류가 발생했습니다.');
+    }
+    return null;
+  }
+};
+
+export default getGroupNotification;
