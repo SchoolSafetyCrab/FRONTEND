ba6656b7bb181443d0b8e8c2bb5fbf015666f283
고승희 <bookshelf1234@ewhain.net>
2024-06-25 22:10:50 +0900

    Feat: 유저 role에 따라 다른 그룹 페이지로 이동

--- src/pages/groupPage/GroupPage.tsx
+++ src/pages/groupPage/GroupPage.tsx
@@ -1,28 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import { useAtomValue } from 'jotai';
 import StudentGroupPage from '@pages/group/StudentGroupPage';
 import TeacherGroupPage from '@pages/group/TeacherGroupPage';
-// import axios from 'axios'; // axios를 사용하여 API 호출
+import userInfoAtom from '../../store/userInfo/UserFindInfo';
 
 export default function GroupPage() {
   const [userRole, setUserRole] = useState<string | null>(null);
-  // const [loading, setLoading] = useState(true);
+  const info = useAtomValue(userInfoAtom);
 
   useEffect(() => {
-    // 유저 정보를 가져오는 API 호출
-    // const fetchUserRole = async () => {
-    //   try {
-    //     const response = await axios.get('/api/user-info'); // 실제 API 엔드포인트로 변경
-    //     const { role } = response.data; // role 값을 받아옴 (예: 'student' 또는 'teacher')
-    //     setUserRole(role);
-    //   } catch (error) {
-    //     console.error('Failed to fetch user role:', error);
-    //   }
-    // };
-
-    // fetchUserRole();
-
-    // 임시로 학생 역할 설정
-    setUserRole('student');
+    if (info.role === 'ROLE_STUDENT') {
+      setUserRole('student');
+    } else if (info.role === 'ROLE_TEACHER') {
+      setUserRole('teacher');
+    } else {
+      setUserRole('parents');
+    }
   }, []);
 
   if (userRole === null) {
