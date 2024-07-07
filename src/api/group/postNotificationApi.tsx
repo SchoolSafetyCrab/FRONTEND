71b8c228eb0e442adcf14edb3c669762743ce6bd
고승희 <bookshelf1234@ewhain.net>
2024-07-07 20:08:00 +0900

    Fix: 공지사항 작성 api 오류 수정

--- src/api/group/postNotificationApi.tsx
+++ src/api/group/postNotificationApi.tsx
@@ -1,17 +1,18 @@
 import axios from 'axios';
+import API_BASE_URL from '../Apiconfig';
 
 export interface NotificationInfo {
   groupId: number;
   title: string;
   detail: string;
-  endDate: Date;
+  endDate: string;
 }
 
 export const postNotification = async (notificationInfo: NotificationInfo): Promise<boolean> => {
   let result: boolean = false;
   const accessToken = localStorage.getItem('ACCESS_TOKEN');
   await axios
-    .post(`http://localhost:8081/api/teacher/create/notification`, notificationInfo, {
+    .post(`${API_BASE_URL}api/teacher/create/notification`, notificationInfo, {
       headers: {
         Authorization: `${accessToken}`,
       },
@@ -23,7 +24,7 @@ export const postNotification = async (notificationInfo: NotificationInfo): Prom
     .catch((err) => {
       if (axios.isAxiosError(err)) {
         console.log(err);
-        alert(err.response?.data?.errorMessage || '알 수 없는 오류가 발생했습니다.');
+        alert(err.response?.data?.errorMessage || 'axios 오류가 발생했습니다');
       } else {
         alert('알 수 없는 오류가 발생했습니다.');
       }
